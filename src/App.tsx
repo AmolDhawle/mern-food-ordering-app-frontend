7a922712fe5790dd1a9cf427a199080be8504d1e
Amol Dhawle <amoldhawle2@gmail.com>
2024-03-01 12:55:23 +0530

    Created Details Page and added menuitem, restaurantinfo and order summary to it

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import UserProfilePage from './pages/UserProfilePage';
 import ProtectedRoutes from './auth/ProtectedRoutes';
 import ManageRestaurantPage from './pages/ManageRestaurantPage';
 import SearchPage from './pages/SearchPage';
+import DetailPage from './pages/DetailsPage';
 
 const App = () => {
   return (
@@ -29,6 +30,14 @@ const App = () => {
         element={
           <Layout showHero={false}>
             <SearchPage />
+          </Layout>
+        }
+      />
+       <Route
+        path="/detail/:restaurantId"
+        element={
+          <Layout showHero={false}>
+            <DetailPage />
           </Layout>
         }
       />
